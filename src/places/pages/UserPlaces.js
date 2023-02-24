390c4f529064711148574c477f6cb3bbf0a66b73
Ian Ho <ianho@ianho.local>
2023-02-25 00:57:48 +0800

    first commit

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "one of the most famous buildings in the world",
+    imageUrl:
+      "https://cdn.britannica.com/73/114973-050-2DC46083/Midtown-Manhattan-Empire-State-Building-New-York.jpg",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484445,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "one of the most famous buildings in the world",
+    imageUrl:
+      "https://cdn.britannica.com/73/114973-050-2DC46083/Midtown-Manhattan-Empire-State-Building-New-York.jpg",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484445,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
