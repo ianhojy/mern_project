eff9c6f0a106567132320eea5d54f0f1ba866c21
Ian Ho <ianho@ianho.local>
2023-02-25 10:22:04 +0800

    Adding UpdatePlace.js

---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "one of the most famous buildings in the world",
+    imageUrl:
+      "https://cdn.britannica.com/73/114973-050-2DC46083/Midtown-Manhattan-Empire-State-Building-New-York.jpg",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484445,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "one of the most famous buildings in the world",
+    imageUrl:
+      "https://cdn.britannica.com/73/114973-050-2DC46083/Midtown-Manhattan-Empire-State-Building-New-York.jpg",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484445,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const UpdatePlace = () => {
+  const placeId = userParams().placeId;
+
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={() => {}}
+        value={identifiedPlace.title}
+        valid={true}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="Title"
+        label="Title"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min 5 characters)."
+        onInput={() => {}}
+        value={identifiedPlace.description}
+        valid={true}
+      />
+      <Button type="submit" disabled={true}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+
+  return <h2>Update Place</h2>;
+};
+
+export default UpdatePlace;
